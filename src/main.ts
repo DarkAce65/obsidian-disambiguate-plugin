086718b8db20f87373434aa3698f42c27180b6ee
Taha Vasowalla <tvdood99@gmail.com>
2024-03-31 20:49:12 -0400

    Register DisambiguationView and hook into link clicks

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,38 @@
 import { around } from 'monkey-around';
 import { Plugin, Workspace, getLinkpath } from 'obsidian';
 
-import UnresolvedLinkModal from './UnresolvedLinkModal.tsx';
+import DisambiguationView, {
+  DISAMBIGUATION_VIEW_TYPE,
+  DisambiguationViewState,
+} from './DisambiguationView.tsx';
 
 class DisambiguatePlugin extends Plugin {
   onload(): void {
+    this.registerView(DISAMBIGUATION_VIEW_TYPE, (leaf) => new DisambiguationView(leaf));
+    this.registerHoverLinkSource(DISAMBIGUATION_VIEW_TYPE, {
+      defaultMod: false,
+      display: 'Disambiguation view',
+    });
+
     this.register(
       around(Workspace.prototype, {
         openLinkText: (existingFunction) => {
           const app = this.app;
 
           return async function (this: Workspace, ...args) {
-            const [linktext, sourcePath] = args;
+            const [linktext, sourcePath, newLeaf] = args;
             const bestMatch = app.metadataCache.getFirstLinkpathDest(
               getLinkpath(linktext),
               sourcePath,
             );
 
             if (bestMatch === null) {
-              new UnresolvedLinkModal(app, linktext).open();
-              return Promise.resolve();
+              const state: DisambiguationViewState = { linktext, sourcePath, newLeaf };
+              return this.getLeaf(newLeaf).setViewState({
+                type: DISAMBIGUATION_VIEW_TYPE,
+                active: true,
+                state,
+              });
             }
             return existingFunction.apply(this, args);
           };
